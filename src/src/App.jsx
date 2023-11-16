0a40e71aecd1678892c5cf3c76bbdf9a9989c53f
Yuvraj <yuvraj8847.11@gmail.com>
2023-11-17 00:19:07 +0530

    setting up

---
+++ src/src/App.jsx
@@ -0,0 +1,23 @@
+import {Route, Routes, BrowserRouter} from 'react-router-dom';
+import Home from './pages/Home';
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+import Profile from "./pages/About";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/sign-in" element={<SignIn/>}></Route>
+        <Route path="/sign-up" element={<SignUp/>}></Route>
+        <Route path="/about" element={<About/>}></Route>
+        <Route path="/profile" element={<Profile/>}></Route>
+      </Routes>
+    </BrowserRouter>
+
+  )
+}
+
+export default App
