787e7c1d5f388ec65839ff774cb0e059cf5db4e7
Yuvraj <yuvraj8847.11@gmail.com>
2023-11-17 15:08:54 +0530

    signup completed

--- api/index.js
+++ api/index.js
@@ -8,6 +8,7 @@ import cookieParser from 'cookie-parser';
 
 dotenv.config();
 const app = express();
+app.use(express.json());
 
 mongoose.connect(process.env.MONGO)
 .then(() => {
@@ -18,6 +19,17 @@ mongoose.connect(process.env.MONGO)
 })
 
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error.';
+    return res.status(statusCode).json({
+        success : false,
+        statusCode,
+        message
+    });
+})
 
 app.listen(3000, () => {
     console.log("server is running on port 3000!");
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom';
 export default function Header() {
   return (
     <header className='bg-slate-200 shadow-md'>
-      <div className='flex justify-between items-center max-w-6xl max-auto p-3'>
+      <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
 
         <Link to='/'>
           <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
